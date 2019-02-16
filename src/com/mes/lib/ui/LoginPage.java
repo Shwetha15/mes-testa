6554b985f7854b23ffcc152eefe0105673cb444c
Shwetha15 <shwethashetty115@gmail.com>
2019-02-16 12:00:44 +0530

    Added content of framework

---
+++ src/com/mes/lib/ui/LoginPage.java
@@ -0,0 +1,50 @@
+package com.mes.lib.ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+	
+	WebDriver driver;
+	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	public void waitForLoginPageToLoad()
+	{
+		WebDriverWait wait=new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("whiteBoxWithLogo"))));
+		
+	}
+	
+	public WebElement getUserNameTextBox()
+	{
+		WebElement element = driver.findElement(By.id("username"));
+		return element;
+	}
+	
+	public WebElement getPasswordTextBox()
+	{
+		return driver.findElement(By.name("pwd"));
+	
+	}
+	
+	public WebElement getLoginButton()
+	{
+		return driver.findElement(By.id("loginButton"));
+	
+	}
+	
+	public WebElement getLoginErrorMsg()
+	{
+		return driver.findElement(By.xpath("//span[text()='Username or Password is invalid. Please try again.']"));
+	
+	}
+
+
+}
